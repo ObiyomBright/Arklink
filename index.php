221d13f1843acdbc82781931be17da93bf62fa6e
ObiyomBright <brightobiyom2@gmail.com>
2024-12-04 07:10:14 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,19 +1,21 @@
 <?php
 include 'database.php';
+header('Content-Type: application/json');
 
 $sql = "SELECT id, img, price, size, producer FROM products";
 $result = mysqli_query($conn, $sql);
-$items = [];
+$products = array();
 
+//Fetch products and store them in products array
 if(mysqli_num_rows($result)> 0){
     while($row = mysqli_fetch_assoc($result)){
-        $items[] =  $row; 
+        $products[] =  $row; 
     }
 }
+
+//Return as JSON
+echo json_encode($products);
+
 //Close connection
 mysqli_close($conn);
-//Return as JSON
-header('Content-Type: application/json');
-echo json_encode($items);
-
-?>
+?>   
